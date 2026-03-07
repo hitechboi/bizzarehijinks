bf280587df3d8308609c7c429c8486f40fc1e584
hitech <113853895+hitechboi@users.noreply.github.com>
2026-03-06 18:16:01 -0600

    Update hinjikse.lua

--- hinjikse.lua
+++ hinjikse.lua
@@ -82,11 +82,11 @@ updates:Div("UPDATE LOG")
 updates:Log({
     "STAR MY POST !",
     "> v1.5.6 - Dynamic title positioning",
-    "> v1.5.2 - Removed resize handle",
-    "> v1.5.1 - Matcha tick() fix",
-    "> v1.5.0 - Widgets scale with menu",
-    "> v1.4.9 - Slider close fix",
-    "> v1.4.0 - Smooth collapse + minimize bar",
+    "> v1.5.6 - Removed resize handle",
+    "> v1.5.6 - Added themes in Misc",
+    "> v1.5.6 - Widgets scale with menu",
+    "> v1.5.6 - Slider close fix",
+    "> v1.5.6 - Smooth collapse + minimize bar",
     "> hi :p"
 }, true)
 
